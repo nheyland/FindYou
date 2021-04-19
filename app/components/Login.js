2a3f83daff32e95847ae2b8d643ab13215295aeb
Nick Heyland <nheyland@gmail.com>
2021-04-19 19:59:18 -0400

    first commit

---
+++ app/components/Login.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { View, Text, StyleSheet } from "react-native";
+
+const Login = (props) => {
+  return (
+    <View style={styles.container}>
+      <Text>Hello Wolrd</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {},
+});
+
+export default Login;
